caee5300a681ecc9242e57312634bb29768b4973
XNIL6X 404 <108590422+xnil6x404@users.noreply.github.com>
2024-12-28 16:29:47 +0600

    Update updateReadme.js

--- updateReadme.js
+++ updateReadme.js
@@ -88,10 +88,16 @@ import("node-fetch").then(({ default: fetch }) => {
 ---
 
 ### 📈 GitHub Activity
-
-![GitHub Activity](https://github-readme-activity-graph.cyclic.app/graph?username=xnil6x404&theme=github)
-
----
+<p align="center">
+  <img src="https://github-profile-trophy.vercel.app/?username=xnil6x404">
+  <br>
+  <br>
+<a href="https://github.com/ntkhang03">
+    <img align="center" src="https://github-readme-stats.vercel.app/api?username=xnil6x404&show_icons=true&theme=github_dark&line_height=27" alt="XNIL6X404's github stats" style="margin-top: 10px;"/>
+  </a>
+  <br>
+  <br>
+  
 
 ### 🌐 Most Used Languages
 ![Top Languages](https://github-readme-stats.vercel.app/api/top-langs/?username=xnil6x404&langs_count=10&layout=compact&theme=radical)
@@ -99,8 +105,8 @@ import("node-fetch").then(({ default: fetch }) => {
 ---
 
 ### 📋 Pinned Repositories
-[![My Repository](https://github-readme-stats.vercel.app/api/pin/?username=xnil6x404&repo=my-repo&theme=radical)](https://github.com/xnil6x404/my-repo)
-[![Another Repo](https://github-readme-stats.vercel.app/api/pin/?username=xnil6x404&repo=another-repo&theme=radical)](https://github.com/xnil6x404/another-repo)
+[![My Repository](https://github-readme-stats.vercel.app/api/pin/?username=xnil6x404&repo=my-repo&theme=radical)](https://github.com/xnil6x404/V5)
+[![Another Repo](https://github-readme-stats.vercel.app/api/pin/?username=xnil6x404&repo=another-repo&theme=radical)](https://github.com/xnil6x/GoatBot-V2.git)
 
 ---
 
