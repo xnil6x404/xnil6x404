da871c3cb2eb363c7195007fd1a126f56c9f4e8d
XNIL6X 404 <108590422+xnil6x404@users.noreply.github.com>
2024-12-28 16:18:44 +0600

    Update updateReadme.js

--- updateReadme.js
+++ updateReadme.js
@@ -9,53 +9,51 @@ import("node-fetch").then(({ default: fetch }) => {
     .then((data) => {
       const readmeContent = `
 
-##  👋 Hi, I'm [${data.login}!](https://www.facebook.com/xnil6x404) 👋
-<h2 align="center">I am a student and i am from BANGLADESH.
-Welcome to my GitHub profile! Here's a quick overview of my activities and stats.</h2>
+# 👋 Hi, I'm [${data.login}](https://www.facebook.com/xnil6x404) 👋
+<h2 align="center">I am a student from BANGLADESH.</h2>
+<h3 align="center">Welcome to my GitHub profile! Here's a quick overview of my activities and stats.</h3>
 
-<p align="left"> <img src="https://komarev.com/ghpvc/?username=xnil6x404&label=Views&color=blue&style=plastic" alt="ntkhang" /> </p>
+<p align="center"> 
+  <img src="https://komarev.com/ghpvc/?username=xnil6x404&label=Profile%20Views&color=blue&style=plastic" alt="Profile views" /> 
+</p>
 
-<a href="https://twitter.com/xnil6x">
-  <img align="left" alt="XNIL6X's Twitter" width="30px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg"/>
-</a>
-<a href="https://github.com/xnil6x404">
-  <img align="left" alt="XNIL6X's Github" width="30px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/github.svg" />
-</a>
-<a href="https://instagram.com/xnil6x">
-  <img align="left" alt="XNIL6X's Instagram" width="30px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/instagram.svg" />
-</a>
-<a href="https://www.facebook.com/xnil6x404">
-  <img align="left" alt="XNIL6X's Facebook" width="30px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/facebook.svg" />
-<br/>
-<br/>
+<p align="center">
+  <a href="https://twitter.com/xnil6x">
+    <img alt="Twitter" width="40" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg"/>
+  </a>
+  <a href="https://github.com/xnil6x404">
+    <img alt="GitHub" width="40" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/github.svg" />
+  </a>
+  <a href="https://instagram.com/xnil6x">
+    <img alt="Instagram" width="40" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/instagram.svg" />
+  </a>
+  <a href="https://www.facebook.com/xnil6x404">
+    <img alt="Facebook" width="40" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/facebook.svg" />
+  </a>
+</p>
+
+<br/><br/>
 
 ![Profile Picture](${data.avatar_url})
 
+---
 
 ## 📫 How to reach me:
 
-| Facebook | Telegram | Twitter | Youtube | 
-| :---: | :---: | :---: | :---: |
-| [![image](https://raw.githubusercontent.com/ntkhang03/ntkhang03/main/sources/qr-fb.svg)](https://www.facebook.com/xnil6x404) <p><b><a href="https://www.facebook.com/xnil6x404">XNIL6X</a><b></p> | [![image](https://raw.githubusercontent.com/ntkhang03/ntkhang03/main/sources/qr-tele.svg)](https://t.me/xnil6x) <p><b><a href="https://t.me/xnil6x">XNIL6X</a><b></p> | [![image](https://raw.githubusercontent.com/ntkhang03/ntkhang03/main/sources/qr-tw.svg)](https://twitter.com/xnil6x) <p><b><a href="https://twitter.com/xnil6x">XNIL6X</a><b></p>
-<!-- email -->
+| Platform | Link |
+| :---: | :---: |
+| [Facebook](https://www.facebook.com/xnil6x404) | ![QR](https://raw.githubusercontent.com/ntkhang03/ntkhang03/main/sources/qr-fb.svg) |
+| [Telegram](https://t.me/xnil6x) | ![QR](https://raw.githubusercontent.com/ntkhang03/ntkhang03/main/sources/qr-tele.svg) |
+| [Twitter](https://twitter.com/xnil6x) | ![QR](https://raw.githubusercontent.com/ntkhang03/ntkhang03/main/sources/qr-tw.svg) |
+
 **✉️ Email:** xnilxhowdhury@gmail.com
 
 [![Twitter: XNIL6X](https://img.shields.io/twitter/follow/xnil6x?style=social)](https://twitter.com/xnil6x)
-[![GitHub: XNIL6X 404](https://img.shields.io/github/followers/xnil6x404?label=follow&style=social)](https://github.com/xnil6x404)
+[![GitHub: XNIL6X](https://img.shields.io/github/followers/xnil6x404?label=follow&style=social)](https://github.com/xnil6x404)
 
-<!-- ngôn ngữ đã học -->
-**Learned Languages and Frameworks:**
-<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" style="background: #000;"></code>
-<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"></code>
-<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"></code>
-<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg"></code>
+---
 
-**Tools used:**
-<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"></code>
-<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg"></code>
-<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg"></code>
-
-### 🌟 About Me
+## 🌟 About Me
 - **Username:** [${data.login}](${data.html_url})
 - **GitHub ID:** ${data.id}
 - **Member Since:** ${new Date(data.created_at).toDateString()}
@@ -64,20 +62,34 @@ Welcome to my GitHub profile! Here's a quick overview of my activities and stats
 ---
 
 ### 🚀 My GitHub Stats
-- **Public Repositories:** ${data.public_repos}  
-- **Public Gists:** ${data.public_gists}  
-- **Followers:** ${data.followers}  
-- **Following:** ${data.following}  
+- **Public Repositories:** ${data.public_repos}
+- **Public Gists:** ${data.public_gists}
+- **Followers:** ${data.followers}
+- **Following:** ${data.following}
 
 ---
 
-### 📬 Contact Me
+### 🛠️ Technologies & Tools:
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"></code>
+<code><img height="30" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"></code>
+<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"></code>
+<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg"></code>
+
+**Other Tools:**
+<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"></code>
+<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg"></code>
+<code><img height="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg"></code>
+
+---
+
+### 📬 Contact Me:
 Feel free to check out my profile at [GitHub Profile](${data.html_url}).
 
 ---
 
 ### 💡 Fun Fact
 GitHub has been part of my journey since **${new Date(data.created_at).getFullYear()}**, and I'm always learning and sharing new projects.
+
 `;
 
       // Write the updated README.md file
